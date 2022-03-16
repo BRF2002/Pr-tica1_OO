78d4233c27dcf3b9e3e65a92778be71da6312fbe
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-03-16 14:36:12 -0300

    Delete Pratica1 directory

--- Pratica1/src/ex1/Transacao.java
+++
@@ -1,9 +0,0 @@
-package ex1;
-
-public class Transacao {
-
-   private String DtaTransacao;
-   private byte TypeTransacao;
-   private double Valor;
-   private byte FormPag;
-}
--- Pratica1/src/ex2/EmpregoFaculdade.java
+++
@@ -1,9 +0,0 @@
-package ex2;
-
-public class EmpregoFaculdade {
-		private String nome;
-		private double salario;
-		private int hrsAula;
-		
-		
-}
