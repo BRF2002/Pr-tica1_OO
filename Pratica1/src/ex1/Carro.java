9153cb6b041407c8f9a0d4168e767bbe4b3ba042
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-03-15 22:10:21 -0300

    Add files via upload

---
+++ Pratica1/src/ex1/Carro.java
@@ -0,0 +1,11 @@
+package ex1;
+
+public class Carro {
+
+    private String Carro;
+    private String fabricante; 
+    private String cor; 
+    private double precovenda;
+    private int anoCar; 
+}
+
---
+++ Pratica1/src/ex2/EmpregoFaculdade.java
@@ -0,0 +1,9 @@
+package ex2;
+
+public class EmpregoFaculdade {
+		private String nome;
+		private double salario;
+		private int hrsAula;
+		
+		
+}
