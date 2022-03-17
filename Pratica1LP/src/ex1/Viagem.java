3bfba3be481847ed8d75a9bac98689ba22c5ef21
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-03-16 22:46:54 -0300

    Add files via upload

---
+++ Pratica1LP/src/ex1/Viagem.java
@@ -0,0 +1,26 @@
+package ex1;
+
+public class Viagem {
+public static void main(String[] args) {
+		
+		float horaViagem, veloMedia, kmLitro, distancia;
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Insira sua velocidade média:");
+		veloMedia = ler.nextFloat();
+		
+		System.out.println("Insira a hora gasta:");
+		horaViagem = ler.nextFloat();
+		
+		System.out.println("Insira quantos KM o seu carro faz por litro:");
+		kmLitro = ler.nextFloat();
+		
+		distancia = veloMedia * horaViagem;
+		
+		System.out.printf("O consumo final de combustível foi de: %.2f", distancia/kmLitro, "Litros");
+
+	}
+
+}
+
